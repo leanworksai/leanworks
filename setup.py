722c811f67b147f73c38febb0a927815d7086ba5
Royisaboy <zhuyanfu0712@gmail.com>
2025-05-18 20:21:31 -0700

    fixed memory issue

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="leanworks",
-    version="0.2.1",
+    version="0.2.2",
     packages=find_packages(),
     install_requires=[
         "pinecone-client==5.0.1",
