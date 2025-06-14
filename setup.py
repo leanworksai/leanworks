7984aa3d00bbc5f8664d60a8ac1cfe4f34b57ae1
Royisaboy <zhuyanfu0712@gmail.com>
2025-06-14 14:51:03 -0700

    enabled multi-tenants supports by auto-detect client name based on user id

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="leanworks",
-    version="0.2.4",
+    version="0.2.5",
     packages=find_packages(),
     install_requires=[
         "pinecone-client==5.0.1",
