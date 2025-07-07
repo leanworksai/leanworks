2457f479ba232faa0295f085a068e9dfd235441a
Royisaboy <zhuyanfu0712@gmail.com>
2025-07-06 22:18:37 -0400

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="leanworks",
-    version="0.2.7",
+    version="0.2.8",
     packages=find_packages(),
     install_requires=[
         "pinecone==7.2.0",
