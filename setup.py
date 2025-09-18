360318babdca985b087a9cc53f37c82974759ac5
Royisaboy <zhuyanfu0712@gmail.com>
2025-09-17 19:58:05 -0700

    Update version to 0.3.10 in setup.py for the next release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="leanworks",
-    version="0.3.9",
+    version="0.3.10",
     packages=find_packages(),
     install_requires=[
         "pinecone==7.2.0",
