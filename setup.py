4f916c626459efaf05afaa12bbeaf1f01fd2f95d
yanfuzhu94 <yanfu@leanworks.ai>
2025-11-17 22:04:53 -0800

    Update version to 0.3.32 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="leanworks",
-    version="0.3.31",
+    version="0.3.32",
     packages=find_packages(),
     install_requires=[
         "pinecone==7.2.0",
